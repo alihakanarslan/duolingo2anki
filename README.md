f6826f3d716507e1bc35a153994b0396b17b21fa
aliharslan0 <aliharslan0@gmail.com>
2022-11-19 18:29:31 +0300

    reset history

---
+++ README.md
@@ -0,0 +1,8 @@
+# duolingo2anki
+Pull your Duolingo vocabulary.
+
+## Usage
+1. Fill the empty fields in main.py
+   * For `NOTE_TYPE`, `WORD_FIELD`, `MEANING_FIELD` and `SPELLING_FIELD` you have to create custom note type in Anki or you can edit main.py.
+2. Install **AnkiConnect** add-on.
+3. **Run**.
---
+++ main.py
@@ -0,0 +1,79 @@
+from json import dumps
+
+from duolingo import Duolingo
+from requests import get, post
+from selectolax.parser import HTMLParser
+
+USERNAME = ''
+PASSWORD = ''
+MAIN_LANGUAGE = ''
+TARGET_LANGUAGE = ''
+DECK_NAME = f'[duolingo2anki] {MAIN_LANGUAGE} - {TARGET_LANGUAGE}'
+NOTE_TYPE = ''
+WORD_FIELD = ''
+MEANING_FIELD = ''
+SPELLING_FIELD = ''
+
+
+def log(message: str):
+    print(f'[duolingo2anki] - {message}')
+
+
+def request(action, **params):
+    return {'action': action, 'params': params, 'version': 6}
+
+
+def invoke(action, **params):
+    data = dumps(request(action, **params))
+    response = post('http://localhost:8765', data=data.encode()).json()
+    return response
+
+
+def main():
+    log('Connecting to the API...')
+    api = Duolingo(USERNAME, PASSWORD)
+
+    log('Getting words...')
+    words = [i['word_string'] for i in api.get_vocabulary(TARGET_LANGUAGE)['vocab_overview']]
+
+    url = F'https://duome.eu/vocabulary/{MAIN_LANGUAGE}/{TARGET_LANGUAGE}'
+    content = get(url).content
+
+    parser = HTMLParser(content)
+    lis = parser.css_first('.plain').css('li:not(.single)')
+
+    log('Getting meanings and spellings...')
+    vocabulary = []
+    for li in lis:
+        if (info := li.css_first('.wA')).text() in words:
+            title = info.attributes['title']
+            spelling, meaning = title.split(']')
+            vocabulary.append(
+                {
+                    'word': info.text().strip(),
+                    'spelling': spelling[1:].strip(),
+                    'meaning': meaning.strip()
+                }
+            )
+
+    invoke('createDeck', deck=DECK_NAME)
+
+    log('Creating notes...')
+    notes = [
+        {
+            'deckName': DECK_NAME,
+            'modelName': NOTE_TYPE,
+            'fields': {
+                WORD_FIELD: w['word'],
+                MEANING_FIELD: w['meaning'],
+                SPELLING_FIELD: w['spelling']
+            }
+        } for w in vocabulary
+    ]
+
+    invoke('addNotes', notes=notes)
+    log('Done.')
+
+
+if __name__ == '__main__':
+    main()
